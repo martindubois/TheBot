f0f318abf445bd6457218059ba9af76f7fef9b9e
Martin Dubois <mdubois@kms-quebec.com>
2022-08-24 14:18:28 -0400

    front-end - initial commit

---
+++ front-end/src/index.js
@@ -0,0 +1,23 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   TheBot
+// File      front-end/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
---
+++ front-end/src/pages/Camera.js
@@ -0,0 +1,13 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   TheBot
+// File      front-end/src/pages/Camera.js
+
+const Home = () =>
+{
+    return <h1>Camera</h1>
+}
+
+export default Home;
