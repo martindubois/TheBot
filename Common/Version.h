e9d0cb42bda7931cba675a42958db775916cc29d
Martin Dubois <mdubois@kms-quebec.com>
2022-08-30 11:13:54 -0400

    0.0.1-front-end

--- Common/Version.h
+++ Common/Version.h
@@ -15,9 +15,9 @@
 // Constants
 // //////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC  0,0,0,0
-#define VERSION_STR "0.0.0.0"
+#define VERSION_RC  0,0,1,0
+#define VERSION_STR "0.0.1.0"
 
 #ifdef __cplusplus
-    KMS_VERSION("Dev");
+    KMS_VERSION("front-end");
 #endif
